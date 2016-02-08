4ee34a90d3f41415e86ae2e97ffa55deac6aff7a
on1x <on1x1990@yandex.ru>
2016-02-08 13:08:22 +0400

    Start Project

---
+++ Main.java
@@ -0,0 +1,16 @@
+package ru.artemryzhenkov;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Artem2011 on 08.02.2016.
+ */
+public class Main extends Application {
+    public static void main (String args[]){
+        launch(args);
+    }
+
+    @Override
+    public void start (Stage primaryStage){}
+}
