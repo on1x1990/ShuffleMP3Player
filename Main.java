a4e5e5e1627a6d6ef72111fb4e85f49df5fdb1fc
on1x <on1x1990@yandex.ru>
2016-02-08 19:39:01 +0400

    add something elements

--- Main.java
+++ Main.java
@@ -1,21 +1,18 @@
 package ru.artemryzhenkov;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.event.*;
+import javafx.geometry.*;
 import javafx.scene.control.*;
-import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.GridPane;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.media.*;
 import javafx.scene.paint.Color;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-
+import javafx.stage.*;
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
 
 
@@ -23,7 +20,7 @@ import java.util.List;
  * Created by Artem2011 on 08.02.2016.
  */
 
-enum AppState {UNKNOWN;}
+enum AppState {READY_TO_PLAY, UNKNOWN;}
 
 public class Main extends Application {
     // app var's
@@ -31,6 +28,7 @@ public class Main extends Application {
     final int HEIGHT = 700;
     AppState appState = AppState.UNKNOWN;
     List<File> filesList;
+    MediaView mv = new MediaView();
 
     // launch app
     public static void main (String args[]){
@@ -50,14 +48,23 @@ public class Main extends Application {
         rootNode.setPadding(new Insets(5, 5, 5, 5));
         rootNode.setGridLinesVisible(false);
 
-        // embedded grid panes
-        // --------
+            // embedded grid panes
+            // --------
+
         // topPane
         final GridPane topPane = new GridPane();
         topPane.setMinSize(0.7*WIDTH, 0.15*HEIGHT);
+        topPane.setAlignment(Pos.CENTER);
+        topPane.setHgap(5);
+        topPane.setVgap(5);
+        topPane.setPadding(new Insets(5, 5, 5, 5));
         Background topPaneBG = new Background(new BackgroundFill(Color.AQUAMARINE, new CornerRadii(10.0), null));
         topPane.setBackground(topPaneBG);
         rootNode.add(topPane, 0, 0);
+
+        final Label stateLabel = new Label("State: " + appState.toString());
+        topPane.add(stateLabel, 0, 0);
+
         // mainPane
         final GridPane mainPane = new GridPane();
         mainPane.setAlignment(Pos.CENTER);
@@ -78,12 +85,25 @@ public class Main extends Application {
         final Label choiseResponse = new Label("Nothing choised");
         mainPane.add(choiseResponse, 0, 1);
 
+        mainPane.add(mv, 0, 3);
+
         choiseButton.setOnAction((ActionEvent ae) -> {
             filesList = fc.showOpenMultipleDialog(primaryStage);
             if (filesList != null) {
+                appState = AppState.READY_TO_PLAY;
+                stateLabel.setText("State: " + appState.toString());
                 for (File file : filesList) {
+                    try {
+                        MediaPlayer mp = new MediaPlayer(new Media(file.toURI().toURL().toString()));
+                        mp.setAutoPlay(true);
+                        mv = new MediaView(mp);
+                        String fileName = file.getName().toString();
+                        fileName = (fileName.length() > 35) ? fileName.substring(0, 34) : fileName;
+                        choiseResponse.setText("Playing " + fileName + "...");
+                    }
+                    catch (MalformedURLException e) {System.out.println(e);}
                 }
-                choiseResponse.setText("Your choise is: ");
+                //choiseResponse.setText("Your choise is: ");
             }
             else choiseResponse.setText("Choose please!");
         });
