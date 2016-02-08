f532214799d84723310e998888b414b44b932964
on1x <on1x1990@yandex.ru>
2016-02-08 17:43:48 +0400

    add background for grids

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -29,20 +33,27 @@ public class Main extends Application {
         rootNode.setHgap(5);
         rootNode.setVgap(5);
         rootNode.setPadding(new Insets(5, 5, 5, 5));
-        rootNode.setGridLinesVisible(true);
+        rootNode.setGridLinesVisible(false);
+
 
         // embedded grid panes
             // topPane
             GridPane topPane = new GridPane();
             topPane.setMinSize(0.7*WIDTH, 0.15*HEIGHT);
+            Background topPaneBG = new Background(new BackgroundFill(Color.AQUAMARINE, new CornerRadii(10.0), null));
+            topPane.setBackground(topPaneBG);
             rootNode.add(topPane, 0, 0);
             // mainPane
             GridPane mainPane = new GridPane();
             mainPane.setMinSize(0.7*WIDTH, 0.4*HEIGHT);
+            Background mainPaneBG = new Background(new BackgroundFill(Color.LEMONCHIFFON, new CornerRadii(10.0), null));
+            mainPane.setBackground(mainPaneBG);
             rootNode.add(mainPane, 0, 1);
             // bottomPane
             GridPane bottomPane = new GridPane();
             bottomPane.setMinSize(0.7*WIDTH, 0.15*HEIGHT);
+            Background bottomPaneBG = new Background(new BackgroundFill(Color.SKYBLUE, new CornerRadii(10.0), null));
+            bottomPane.setBackground(bottomPaneBG);
             rootNode.add(bottomPane, 0, 2);
 
         Scene primaryScene = new Scene(rootNode, WIDTH, HEIGHT);
